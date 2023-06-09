1c53738c2be7efe67429224f836030b35e32cc61
lkocheny <130501332+lkocheny@users.noreply.github.com>
2023-06-08 17:08:34 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -46,9 +46,9 @@ def metrics(inputlist):
   for item in inputlist:
     assert isinstance(item,list), f'Parameter must be a list of lists'
     assert len(item) == 2, f'Parameter must be a zipped list'
-    for value in item:
-      assert isinstance(value, int), f'All values in the pair must be an integer'
-      assert value>=0, f'All values in the pair must be greater or equal to 0'
+    #for value in item:
+      #assert isinstance(value, int), f'All values in the pair must be an integer'
+      #assert value>=0, f'All values in the pair must be greater or equal to 0'
   tn = sum([1 if pair==[0,0] else 0 for pair in inputlist])
   tp = sum([1 if pair==[1,1] else 0 for pair in inputlist])
   fp = sum([1 if pair==[1,0] else 0 for pair in inputlist])
